8ca7f28ace32642530501f9cd9c4d8311b2c187d
hejazi-zarkawi <hejazimohammad764@gmail.com>
2026-08-16 17:39:43 +0530

    Understanding the Future and CompletableFuture (Module 18)

--- Module 18/multiThreading/src/main/java/com/example/multiThreading/multiThreading/MultiThreadingApplication.java
+++ Module 18/multiThreading/src/main/java/com/example/multiThreading/multiThreading/MultiThreadingApplication.java
@@ -11,17 +11,19 @@ import java.util.concurrent.*;
 
 @SpringBootApplication
 @Slf4j
-public class MultiThreadingApplication implements CommandLineRunner {
+public class MultiThreadingApplication {
 
-	public static void main(String[] args)  {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 
 		SpringApplication.run(MultiThreadingApplication.class, args);
 
+		learnCF2();
+
+		log.info("Back to main thread");
+
 	}
 
-
-	@Override
-	public void run(String... args) throws Exception {
+	static void learnThread(){
 		ThreadPoolExecutor threadPoolExecutor= new ThreadPoolExecutor(4,6,
 				2,java.util.concurrent.TimeUnit.SECONDS,
 				new ArrayBlockingQueue<>(10),
@@ -39,7 +41,7 @@ public class MultiThreadingApplication implements CommandLineRunner {
 						executor.submit(r);
 					}
 				})
-		;
+				;
 
 		ScheduledThreadPoolExecutor scheduledThreadPoolExecutor= new ScheduledThreadPoolExecutor(6,
 				new ThreadFactory() {
@@ -59,4 +61,74 @@ public class MultiThreadingApplication implements CommandLineRunner {
 //		}
 		log.info("Ending thread {}",Thread.currentThread().getName());
 	}
+
+	static void learnFuture() throws ExecutionException, InterruptedException {
+
+		try  {
+
+			ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+			Future<String> myNameFuture = executorService.submit(() -> getName());
+
+			myNameFuture.get(); // block the calling thread
+			log.info("After nameFuture: {}", Thread.currentThread().getState());
+		}
+		catch(Exception e){
+			throw new ExecutionException(e);
+		}
+	}
+
+
+	static void learnCompletableFuture() {
+		CompletableFuture<String> myNameCF = CompletableFuture
+				.supplyAsync(() -> getName())
+				.thenApply(name -> name.toUpperCase())
+				.thenApply(upperCaseName -> upperCaseName.length())
+				.thenApplyAsync(lengthOfName -> {
+					log.info("Inside method with length");
+					if(true) throw new RuntimeException("Faking an error.");
+					return "length was "+lengthOfName;
+				})
+				.exceptionally((err) -> {
+					return "Default value in case of failure";
+				});
+
+		myNameCF.thenAccept(name -> {
+			log.info("Got the name length: {}", name);
+		});
+
+		log.info("After completing Thread");
+	}
+
+	static void learnCF2() {
+		CompletableFuture<String> nameFuture = CompletableFuture.supplyAsync(() -> getName());
+		CompletableFuture<String> addressFuture = CompletableFuture.supplyAsync(() -> getAddress());
+
+		CompletableFuture.allOf(nameFuture, addressFuture)
+				.thenAccept((v) -> {
+					log.info("Got the name: {} and address here: {}", nameFuture.join(), addressFuture.join());
+				});
+
+//        log.info("Got the name: {} and address here: {}", nameFuture.join(), addressFuture.join());
+	}
+	static String getName() {
+		try {
+			log.info("Inside nameFuture: {}", Thread.currentThread().getState());
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+		return "Anuj";
+	}
+
+	static String getAddress() {
+		try {
+			log.info("Inside Addressfuture: {}", Thread.currentThread().getState());
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+		return "New Delhi";
+	}
+
 }
